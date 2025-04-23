7cec63b3b0e7390e3086d2a7677ec94de16dc5cc
samuelgomeslima <samuelgomeslima@hotmail.com>
2025-04-22 23:47:59 -0300

    Initial commit

---
+++ cross-platform-app/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace cross_platform_app;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
